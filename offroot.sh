f82ab5eb6a6c4324f4f71a6668971c81361e7473
Ddarkover <79401832+Ddarkover@users.noreply.github.com>
2024-06-16 18:41:59 +0500

    Update offroot.sh

--- offroot.sh
+++ offroot.sh
@@ -6,4 +6,4 @@ sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 # Перезапускаем службу SSH
 systemctl restart sshd
 
-echo "Настройки SSH обновлены. Теперь PermitRootLogin установлен в 'no'."
+echo "Настройки SSH обновлены. Пользователь root отключен. Теперь PermitRootLogin установлен в 'no'."
